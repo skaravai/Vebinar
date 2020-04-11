990ef92996aa77addb5b5abc57f91ad6607fcb9c
Karavai <Karavai@ip-192-168-1-106.eu-west-3.compute.internal>
2020-04-11 12:11:09 +0300

    Initial commit My first commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,35 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+
+    @Test
+    public void testOTUS(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        driver.get("https://otus.ru");
+        System.out.println(driver.getTitle()+"=========");
+    }
+
+
+    @AfterTest
+    public void quit(){
+        if(driver != null){
+            driver.quit();
+        }
+    }
+}
